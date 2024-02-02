411af83dc38baa2ade00e71dd76ab0fc44c6c39c
ValiBratu <vali.bratu7@gmail.com>
2024-02-02 23:28:42 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style scoped></style>
